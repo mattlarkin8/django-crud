6e610f5ed1587e575cea621af30e77b5c3b0280f
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-24 22:49:51 -0400

    Created Snack model with all required fields. Added __str__ method for better display of item titles.

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Snack(models.Model):
+    title = models.CharField(max_length=256)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
