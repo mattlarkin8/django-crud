478344a61bfe18dcf460bad045e381b176db63a7
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-25 00:03:40 -0400

    Added snacks to list of installed apps. Added path to templates in the templates section.

--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # local
+    "snacks",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = "snacks_crud_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
