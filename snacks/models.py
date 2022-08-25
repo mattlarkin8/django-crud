82f08f38203417438b4d4ef4fb39ed2ad96d316d
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-25 00:05:40 -0400

    Added function to get absolute url.

--- snacks/models.py
+++ snacks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 class Snack(models.Model):
     title = models.CharField(max_length=256)
@@ -8,3 +9,6 @@ class Snack(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("snack_detail", args=[str(self.id)])
